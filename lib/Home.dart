0cf34ccfa27b9caddc1e1455e39db242034bc372
Marcos <devmarcosvinicius@gmail.com>
2022-07-29 12:13:20 -0400

    work on http requests

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +8,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _resultado = "";
+
+  _recuperarCep() async {
+    var url = Uri.parse("https://viacep.com.br/ws/01001000/json/");
+    var response = await http.get(url);
+
+    if(response.statusCode == 200) {
+      var jsonResponse =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
+      var logradouro = jsonResponse['logradouro'];
+      var bairro = jsonResponse['bairro'];
+      var cep = jsonResponse['cep'];
+
+      setState(() {
+        _resultado = "$logradouro, $bairro, $cep";
+      });
+
+      print('Logradouro: $logradouro');
+
+    }else {
+      print('shit');
+    }
+
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HTTP requests"),
+      ),
+    body: Container(
+    padding: const EdgeInsets.all(40),
+    child: Column(
+    children: <Widget>[
+      Text(_resultado),
+      RaisedButton(
+    onPressed: _recuperarCep,
+    child: const Text("Tap here"),
+    ),
+    ],
+    ),
+    ),
+    );
   }
 }
